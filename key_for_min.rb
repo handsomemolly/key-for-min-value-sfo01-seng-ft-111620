237a8fbad9963276c2617f5b59a82a5a2525a9e3
Ryan Kendig <ryan.kendig@gmail.com>
2020-10-30 16:21:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_value_item = ""
   name_hash.each do |item, value|
-    if suite == :suite_a && name.start_with?("A")
+    if value 
     min_value_item = item
   end
 
