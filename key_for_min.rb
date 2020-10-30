bef6fff89f8f4155f39eacfe41f8f25e1362e276
Ryan Kendig <ryan.kendig@gmail.com>
2020-10-30 16:26:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_value_item = ""
   name_hash.each do |item, value|
-    if value 
+    if value[0] < 
     min_value_item = item
   end
   puts min_value_item
