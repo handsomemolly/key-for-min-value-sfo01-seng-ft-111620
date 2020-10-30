3b3d89cfd439c433f8921e4461527808ace31717
Ryan Kendig <ryan.kendig@gmail.com>
2020-10-30 16:18:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  min_value = ""
   name_hash.each do |item, value|
     if suite == :suite_a && name.start_with?("A")
-    winner = name
+    min_value == 
   end
 
 end
