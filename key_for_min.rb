bfd24bbb2301d2fcda14f1087522c4934be3540f
Ryan Kendig <ryan.kendig@gmail.com>
2020-10-30 16:19:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = ""
+  min_value_item = ""
   name_hash.each do |item, value|
     if suite == :suite_a && name.start_with?("A")
-    min_value = item
+    min_value_item = item
   end
 
 end
